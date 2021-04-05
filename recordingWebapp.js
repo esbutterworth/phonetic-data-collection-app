9f3b345584562458948ae07387d6f2561562556e
Elliot Butterworth <elliot802@gmail.com>
2021-04-04 23:20:54 -0500

    working on 499

--- recordingWebapp.js
+++ recordingWebapp.js
@@ -1,19 +1,28 @@
-// Application for recording sentences for phonetic analysis.
-// Version 0, written in December of 2020, for LING 401.
-// @author Elliot Butterworth
-
-// So jank that these are different values
-const recordingTime = 5000;
-const targetVisibleTime = 4000;
+/* 
+ * Application for recording sentences for phonetic analysis.
+ * Originally written in December of 2020, for LING 401.
+ * Revised Spring 2021 for LING 499.
+ * @author Elliot Butterworth
+ */
 
 // For now, hardcoding the words here, but using JSON so eventually I'll be
 // able to use external files.
 var targetData = '{ "targets": ["The cat is orange.", "The cat jumped over the dog.", "The dog growls at the cat.", "She sent a card from home.", "They went around the park again.", "The vase cracked right after.", "Another subject came in.", "They saw that it was hurt.", "They finished making the pillow fort."]}';
 
+// HTML element for displaying current target 
+var targetDisplayElement;
+
 // TODO it would be nice to have this random, but I think it's more important to have consistent indices.
 var targets = JSON.parse(targetData).targets;
 
-// Functions for audio recording
+// The length of each target
+const targetDisplayTime = 2000;
+// The total length of the recording 
+const recordingTime = targetDisplayTime * targets.length;
+
+/*
+ * Functions for audio recording
+ */
 
 // Create a link to download an audio file
 function createAudioDownloadElement(blobUrl, wordName) {
@@ -28,32 +37,52 @@ function createAudioDownloadElement(blobUrl, wordName) {
 
 // Start the trial.
 function startTrial() {
-    const targetDisplayElement = document.getElementById('target');
-
-    var i = 0;
-
+    targetDisplayElement = document.getElementById('target');
+    
     // Get permission to use the mic:
     navigator.mediaDevices.getUserMedia({audio: true}).then(stream => {
         targetDisplayElement.innerHTML = 'Read the sentences as they appear.';
+        
+        /*
+         * Start the recording.
+         */
+        recordSpeech(stream, blob => {
+          // createAudioDownloadElement(URL.createObjectURL(blob), currentTarget);
+            saveAudio(blob);
+        });
 
-        var timer = setInterval(() => {
-            let currentTarget = targets[i];
+        /*
+         * TODO: instead of using a timer here, just assume that each utterance
+         * is however long. since we'll be recording words instead of whole
+         * sentences, that should work fine.
+         */
+        targets.forEach(currentTarget => {  
             console.log('recording target \"' + currentTarget + '\"');
-            targetDisplayElement.innerHTML = currentTarget;
-            recordSpeech(stream, blob => {
-                // createAudioDownloadElement(URL.createObjectURL(blob), currentTarget);
-                saveAudio(i, blob);
-                i++;
-                targetDisplayElement.innerHTML = '';
-                if (i >= targets.length) {
-                    clearInterval(timer);
-                    stopTrial(stream);
-                }
-            })
-        }, recordingTime);
-
+            
+            // Display the target text    
+            new Promise((resolve, reject => {
+                displayTargetText(currentTarget, () => {});
+                resolve();
+            })).then(() => {
+                // Target is finished 
+                // Next loop iter?? idk rn
+            });
+            
+         });
     }).catch(console.error);
 
+}
+
+/*
+ * Display some text in the target area.
+ * uses a callback function for waiting.
+ */
+function displayTargetText(text, callback) {
+    // Display the text
+    targetDisplayElement.innerHTML = text;
+
+    // Wait, and then call a callback
+    setTimeout(callback, targetDisplayTime);
 }
 
 // Stop the trial
@@ -78,7 +107,7 @@ function recordSpeech(stream, callback) {
         chunks.push(e.data);
     };
 
-    recorder.onstart = e => console.log(recorder.state);;
+    recorder.onstart = e => console.log(recorder.state);
 
     recorder.onstop = e => {
         console.log(recorder.state);
@@ -91,17 +120,18 @@ function recordSpeech(stream, callback) {
         console.log(error);
     }
 
+    // Record for the desired amount of time
     setTimeout(() => {
         recorder.stop();
-    }, targetVisibleTime);
+    }, recordingTime);
 
     // Not sure if I need to put an interval here or not, since it's such a short recording
     recorder.start();
 
 }
 
-function saveAudio(targetIndex, blob) {
-    var filename = targetIndex + "-" + new Date().toISOString();
+function saveAudio(blob) {
+    var filename = new Date().toISOString();
 
     console.log("sending " + filename);
 
@@ -126,3 +156,4 @@ function shuffleArray(array) {
 
     return array;
 }
+
