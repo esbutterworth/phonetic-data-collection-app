aef144433277b40f653f69ea5bb0565accc97577
Elliot Butterworth <elliot802@gmail.com>
2021-04-07 15:08:16 -0500

    working on file conversion

--- process_recordings.py
+++ process_recordings.py
@@ -3,23 +3,12 @@ import parselmouth
 import numpy
 import matplotlib.pyplot as plt
 import seaborn as sns
+import os
+import subprocess
 
-# this is just a test lol
+upload_dir = "./upload"
+wave_dir = "./wave"
+# new filename
+for filename in os.listdir(upload_dir):
+    subprocess.run(['ffmpeg', '-i', filename, ])
 
-sns.set()
-plt.rcParams['figure.dpi'] = 100
-
-snd = parselmouth.Sound("/srv/http/test.webm")
-
-plt.figure()
-plt.plot(snd.xs(), snd.values.T)
-plt.xlim([snd.xmin, snd.xmax])
-plt.xlabel("time [s]")
-plt.ylabel("amplitude")
-plt.show() # or plt.savefig("sound.png"), or plt.savefig("sound.pdf")
-
-###
-###
-# Gonna need shit in .wav for this to work
-###
-###
